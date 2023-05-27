4adf9390fa3af7c8edb1aeeaebf694ee754317e6
Guilherm Henrique <gc3856@gmail.com>
2023-05-27 16:27:09 -0300

    class LinkedList-2

--- main.py
+++ main.py
@@ -14,3 +14,8 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+        
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node

87ee8199980f18a2ed89e14d0a164e03787eb517
Guilherme Dobelin <105359316+GuiDobelin@users.noreply.github.com>
2023-06-10 23:23:16 -0300

    class pessoa-4

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class Pessoa:
     def __init__(self, nome, telefone, especie_interesse, preferencia_animal):
         self.nome = nome
         self.telefone = telefone
+        self.especie_interesse = especie_interesse
 
 class Node:
     def __init__(self, data):

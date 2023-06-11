d99a6ad8ba018d1737564bdc5ff827f05e9cf1e8
Guilherme Dobelin <105359316+GuiDobelin@users.noreply.github.com>
2023-06-11 15:41:51 -0300

    Update pessoa.py-2

--- pessoa.py
+++ pessoa.py
@@ -6,3 +6,9 @@ class Pessoa:
         self.telefone = telefone
         self.especie_interesse = especie_interesse
         self.preferencia_animal = preferencia_animal
+        
+def cadastrar_pessoa():
+    nome = input("Nome da pessoa: ")
+    telefone = input("Telefone da pessoa: ")
+    especie_interesse = input("Espécie de interesse para adoção: ")
+    preferencia_animal = input("Preferência de animal: ")

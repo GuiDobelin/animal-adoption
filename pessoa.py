c2e4bd81047ae19d3d72a6bb0462939c0d67b566
Guilherme Dobelin <105359316+GuiDobelin@users.noreply.github.com>
2023-06-11 15:41:31 -0300

    Update pessoa.py

--- pessoa.py
+++ pessoa.py
@@ -1,1 +1,8 @@
+from main import *
 
+class Pessoa:
+    def __init__(self, nome, telefone, especie_interesse, preferencia_animal):
+        self.nome = nome
+        self.telefone = telefone
+        self.especie_interesse = especie_interesse
+        self.preferencia_animal = preferencia_animal
